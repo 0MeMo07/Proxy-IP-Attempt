89e22ac17083b3976c85ffaba4432c75fd130238
MeMo <omemo0135@gmail.com>
2022-11-24 13:37:17 +0300

    Proxy ip test application with Python
    Proxy IP pull application with python
    
    To inquire about Python or to write software related to your area of interest, dc: memo#0078

---
+++ Proxyip.py
@@ -0,0 +1,41 @@
+import requests
+from bs4 import BeautifulSoup
+from random import choice
+
+
+def smake():
+            print("""
+                
+            ░██████╗███╗░░░███╗░█████╗░██╗░░██╗███████╗
+            ██╔════╝████╗░████║██╔══██╗██║░██╔╝██╔════╝
+            ╚█████╗░██╔████╔██║███████║█████═╝░█████╗░░
+            ░╚═══██╗██║╚██╔╝██║██╔══██║██╔═██╗░██╔══╝░░
+            ██████╔╝██║░╚═╝░██║██║░░██║██║░╚██╗███████╗
+            ╚═════╝░╚═╝░░░░░╚═╝╚═╝░░╚═╝╚═╝░░╚═╝╚══════╝
+
+                                        memo#0078 and Ware#1991
+
+            """)
+def GetProxy():
+    url = 'https://www.sslproxies.org'
+    r = requests.get(url)
+    soup = BeautifulSoup(r.content, 'html5lib')
+    return {'https': choice(list(map(lambda x: x[0]+':'+x[1],list(zip(list(map(lambda x: x.text, soup.find_all('td')[::8])), (map(lambda x: x.text, soup.find_all('td')[1::8])))))))}
+
+def UseProxy(url):
+    while True:
+        try:
+            proxy = GetProxy()
+            r = requests.get(url,proxies=proxy,timeout=5)
+            if r.status_code == 200:
+                print('Çalışan proxy = ', proxy)
+                break
+        except:
+            print('Bu proxy denendi ancak çalışmıyor : ', proxy)
+            pass
+    return r
+
+smake()
+url = 'https://api.ipify.org/'
+x = UseProxy(url)
+print(x.text)
